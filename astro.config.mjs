eea4ae3a25fe1a0bf9e2e52cbbea74e8fd355a5c
Ricardo Contreras <40666926+rcrdevelop@users.noreply.github.com>
2024-06-10 17:44:18 +0000

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel";
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
