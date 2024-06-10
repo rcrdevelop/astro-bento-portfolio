ae45516bec9c9a9ae226989d2272d5fafe3b1aca
Ricardo Contreras <40666926+rcrdevelop@users.noreply.github.com>
2024-06-10 17:54:20 +0000

    update config for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,29 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        //"https://gianmarco.xyz/sitemap-index.xml",
-        //"https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),  
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: [
+      //"https://gianmarco.xyz/sitemap-index.xml",
+      //"https://gianmarco.xyz/sitemap-0.xml",
+    ]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  }), icon(), svelte()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   output: "server",
-  adapter: vercel(),
   vite: {
-    assetsInclude: "**/*.riv",
+    assetsInclude: "**/*.riv"
   },
+  adapter: vercel()
 });
