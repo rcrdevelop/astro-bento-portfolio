94bf4fcc25cc9f9b337cb318b7c2a4048adf4d62
Ricardo Contreras <40666926+rcrdevelop@users.noreply.github.com>
2024-06-11 20:57:22 +0000

    Updates

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,12 @@ export const LINKS = {
   github: "https://github.com/rcrdevelop",
   linkedin: "https://www.linkedin.com/in/ricardo-contreras-rivero-5b807b17a/",
   medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  discord: "https://discordapp.com/users/597205746152636477",
   dribble: "https://dribbble.com/Ladvace_Jace",
+  mail: "mailto:contreras06rj@gmail.com",
+  articlesProject: "",
+  ShopifyStore: "",
+  BotTelegram: ""
 };
 
 export const loaderAnimation = [
